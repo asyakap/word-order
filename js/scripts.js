cfde2ffbe251a3d08430e69392f60866bf68b1a4
Asia Kaplanyan <a.kaplanyan@gmail.com>
2023-01-17 11:28:46 -0800

    add files

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,26 @@
+window.onload = function() {
+  const form = document.querySelector("form");
+  form.onsubmit = function(event) {
+    event.preventDefault();
 
-function handleForm(event) {
-  event.preventDefault();
-  const userSelections = document.querySelectorAll("input:checked");
-  console.log(userSelections);
-  userArray = [];
-  const userSelectionsArray = Array.from(userSelections);
-  console.log(userSelectionsArray);
-  userSelectionsArray.forEach(function(element) {
-    userArray.push(element.value);
-  });
+    let array = [];
+    let array1 = [];
+    let array2 = [];
+    array[0] = document.getElementById("input").value;
+    array1 = array.toString();
+    array1 = array1.split(" ");
 
-  userArray.sort();
-  userArray1 = userArray.toString().toUpperCase().split(",");
-  
-  document.body.innerHTML = "";
-  
-  
-  // create results heading
-  const resultsHeading = document.createElement("h2");
-  resultsHeading.append("Here is what you need to buy:");
-  document.body.append(resultsHeading);
-
-  console.log(userArray1);
-  userArray1.forEach(function(element) {
-    console.log(element);
+    array1.forEach(function(element) {
+      if (element.length > 2) {
+        array2.push(element);
+      }
+    });
+    array2.reverse();
+    array2.join(" ");
+    const element = document.body;
     const paragraph = document.createElement("p");
-    paragraph.append(element);
+    paragraph.append(array2);
     document.body.append(paragraph);
-  });
 
-
+  };
 }
-
-window.addEventListener("load", function() {
-  document.querySelector("form#groceries").addEventListener("submit", handleForm);
-});
